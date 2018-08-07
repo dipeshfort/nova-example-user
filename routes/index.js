1fcf3cdc7ad2e1e695ee2fb43486be67d4efb3ca
dipesh <dipesh.fort@gmail.com>
2018-08-08 01:45:50 +0300

    Task: Add db

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const { Pool } = require('pg');
+
+console.log("DBURL", process.env.DATABASE_URL);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+router.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    res.render('pages/db', result);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
-    title: 'Hello HH!', 
+    title: process.env.APPLICATION_NAME, 
     version: "v1.0.1" 
   });
 });
