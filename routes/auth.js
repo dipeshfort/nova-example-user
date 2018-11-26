8a51032d1346ff6c9907afb563ceb39eb2b2f7b7
dipesh <dipesh.fort@gmail.com>
2018-11-26 21:28:06 +0200

    Add jwt token

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const logger = require('../utils/logger');
 const repository = require('../repository/users-repository');
+const jwt = require('jsonwebtoken');
 
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
@@ -12,7 +13,16 @@ router.post('/login', async (req, res) => {
 
     try {
         const user = await repository.fetchByCredentials(credentials);
-        res.json(user);
+        const token = jwt.sign({
+            user
+        }, process.env.JWT_SECRET, {
+            algorithm: process.env.JWT_ALG,
+            issuer: 'nova-auth',
+            expiresIn: process.env.JWT_EXPIRY
+        });
+        res.json({
+            token
+        });
     } catch (err) {
         error500(err, res);
     }
@@ -36,6 +46,34 @@ router.post('/signup', async (req, res) => {
         res.json(user);
     } catch(err) {
         error500(err, res);
+    }
+});
+
+router.get('/profile', async (req, res) => {
+    const authorization = req.header('authorization');
+
+    if (!authorization) {
+        res.status(401);
+        return res.json({
+            message: 'Unauthorized'
+        });
+    }
+
+    const token = authorization.replace(/Bearer\s/, '');
+    try {
+        const data = jwt.verify(token, process.env.JWT_SECRET);
+        res.json(data.user);
+    } catch(err) {
+        res.status(401);
+        if (err.name && err.name === "TokenExpiredError") {
+            res.json({
+                message: 'Token Expired'
+            });
+        } else {
+            res.json({
+                message: 'Verification failed'
+            });
+        }
     }
 });
 
